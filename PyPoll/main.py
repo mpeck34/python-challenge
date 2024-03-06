79d1f8b487bf0888d0831701ec428514dbd83d48
mpeck34 <mark.peck34@gmail.com>
2024-03-06 18:14:23 +1100

    main functions complete

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,45 @@
+# import what we need
+
+import csv
+import os
+
+# create dict for incoming data
+election_dict = {}
+
+
+cwd = os.getcwd()
+election_data_path = os.path.join("python-challenge","PyPoll","Resources","election_data.csv")
+
+counter = 0 
+with open(election_data_path, 'r') as election_data:
+    csvreader = csv.reader(election_data, delimiter=",")
+    for row in csvreader:
+        election_dict[row[0]] = row[2]
+        counter += 1
+
+total_votes = len(election_dict)-1
+
+print(total_votes)
+
+stockham_vote = 0
+degette_vote = 0
+doane_vote = 0
+
+for ballot_id, candidate in election_dict.items():
+    if candidate == "Charles Casper Stockham":
+        stockham_vote += 1
+    elif candidate == "Diana DeGette":
+        degette_vote += 1
+    elif candidate == "Raymon Anthony Doane":
+        doane_vote += 1
+
+print(stockham_vote)
+print(degette_vote)
+print(doane_vote)
+
+stockham_percent = stockham_vote/total_votes
+degette_percent = degette_vote/total_votes
+doane_percent = doane_vote/total_votes
 
 
 
@@ -5,11 +47,7 @@
 
 
 
+    
 
 
-# with open(budget_data_path, 'r') as budget_data:
-#    csvreader = csv.reader(budget_data, delimiter=",")
-#    for row in csvreader:
-#        date_list.append(row[0])
-#        profit_loss_list.append(row[1])
-        #date_change_dict[row[0]] = row[1]
+
