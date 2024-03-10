cc94f12f3ee4a2a500d4351fc89504e39d7eef2d
mpeck34 <mark.peck34@gmail.com>
2024-03-10 18:26:08 +1100

    Finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,36 +1,38 @@
 # import what we need
 
 import csv
-import os
 
 # create lists for incoming data
 date_list = []
 profit_loss_list = []
 date_change_dict = {}
 
-# read csv to variable
-cwd = os.getcwd()
-#budget_data_path = os.path.join("PyBank","Resources","budget_data.csv")
+# .csv to variable
 budget_data_path = "./PyBank/Resources/budget_data.csv"
 
+# write .csv data to two lists
 with open(budget_data_path, 'r') as budget_data:
     csvreader = csv.reader(budget_data, delimiter=",")
     for row in csvreader:
         date_list.append(row[0])
         profit_loss_list.append(row[1])
 
+# store headers for some reason
 date_list_header = date_list.pop(0)
 profit_loss_list_header = profit_loss_list.pop(0)
 
+# change list from strings to integers
 profit_loss_list_int = [int(x) for x in profit_loss_list]
 
+# total months is the length of the list
 total_months = len(date_list)
 
+# add up total profit
 total_profit = 0
 for num in profit_loss_list_int:
     total_profit += int(num)
 
-
+# create variables to store info about the change in profit/loss
 greatest_profit = 0
 greatest_delta = 0
 greatest_date = ""
@@ -38,6 +40,7 @@ greatest_loss = 0
 greatest_loss_delta = 0
 greatest_loss_date = ""
 
+# iterate and find greatest change in both profit and loss
 for i in range(len(profit_loss_list_int)-1):
     if profit_loss_list_int[i] < profit_loss_list_int[i+1]:
         greatest_delta = profit_loss_list_int[i+1] - profit_loss_list_int[i]
@@ -50,27 +53,29 @@ for i in range(len(profit_loss_list_int)-1):
             greatest_loss = greatest_loss_delta
             greatest_loss_date = date_list[i+1]
 
+# calculate average change in such a way that it matches the asssignment 
 average_change = round((profit_loss_list_int[-1] - profit_loss_list_int[0]) / (len(profit_loss_list_int)-1), 2)
 
-print(total_months)
-print(total_profit)
-print(average_change)
-print(greatest_profit)
-print(greatest_date)
-print(greatest_loss)
-print(greatest_loss_date)
-
-
-output_text_file = open("./PyBank/analysis/FinancialAnalysis.txt","w")
-output_text_file.write("Financial Analysis")
-
+# dashes to make the output look interesting
 dashes = ""
 for i in range(20):
     dashes += "-"
 
-output_text_file.write("\n\n" + dashes)
-output_text_file.write(f"\n\n Total Months: {str(total_months)}")
-output_text_file.write(f"\n\n Total: {str(total_profit)}")
-output_text_file.write(f"\n\n Average Change: {str(average_change)}")
-output_text_file.write(f"\n\n Greatest Increase in Profits: {greatest_date} ({greatest_profit})")
-output_text_file.write(f"\n\n Greatest Decrease in Profits: {greatest_loss_date} ({greatest_loss})")
+# write to .txt file
+with open("./PyBank/analysis/FinancialAnalysis.txt","w") as output_text_file:
+    output_text_file.write("Financial Analysis")
+    output_text_file.write("\n\n" + dashes)
+    output_text_file.write(f"\n\n Total Months: {str(total_months)}")
+    output_text_file.write(f"\n\n Total: {str(total_profit)}")
+    output_text_file.write(f"\n\n Average Change: {str(average_change)}")
+    output_text_file.write(f"\n\n Greatest Increase in Profits: {greatest_date} ({greatest_profit})")
+    output_text_file.write(f"\n\n Greatest Decrease in Profits: {greatest_loss_date} ({greatest_loss})")
+
+# print to terminal
+print("\nFinancial Analysis")
+print("\n" + dashes)
+print(f"\n Total Months: {str(total_months)}")
+print(f"\n Total: {str(total_profit)}")
+print(f"\n Average Change: {str(average_change)}")
+print(f"\n Greatest Increase in Profits: {greatest_date} ({greatest_profit})")
+print(f"\n Greatest Decrease in Profits: {greatest_loss_date} ({greatest_loss})\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,24 +5,34 @@ import os
 # create dict for incoming data
 election_dict = {}
 
+# load .csv file
 cwd = os.getcwd()
 election_data_path = os.path.join("PyPoll","Resources","election_data.csv")
 
+# storing header for some reason
+election_dict_header = []
+
+# write .csv file data into a dictionary
 counter = 0 
 with open(election_data_path, 'r') as election_data:
     csvreader = csv.reader(election_data, delimiter=",")
     for row in csvreader:
+        if row[0] == "Ballot ID":
+            election_dict_header = [row[0], row[1], row[2]]
+            break
+    for row in csvreader:
         election_dict[row[0]] = row[2]
         counter += 1
 
+# calc total votes
 total_votes = len(election_dict)-1
 
-print(total_votes)
-
+# create variables to save respective votes
 stockham_vote = 0
 degette_vote = 0
 doane_vote = 0
 
+# iterate and count respective votes
 for ballot_id, candidate in election_dict.items():
     if candidate == "Charles Casper Stockham":
         stockham_vote += 1
@@ -31,21 +41,56 @@ for ballot_id, candidate in election_dict.items():
     elif candidate == "Raymon Anthony Doane":
         doane_vote += 1
 
-print(stockham_vote)
-print(degette_vote)
-print(doane_vote)
+# calc percent of votes
+stockham_percent = round(((stockham_vote/total_votes) * 100),3)
+degette_percent = round(((degette_vote/total_votes) * 100),3)
+doane_percent = round(((doane_vote/total_votes) * 100),3)
 
-stockham_percent = stockham_vote/total_votes
-degette_percent = degette_vote/total_votes
-doane_percent = doane_vote/total_votes
+# compare respective votes and determine winner
+def calc_winner(list):
+    winner_result = ""
+    temp_winner = 0
+    for candidate in list: 
+        if candidate > temp_winner:
+            temp_winner = candidate
+    if temp_winner == stockham_vote:
+        winner_result = "Charles Casper Stockham"
+    elif temp_winner == degette_vote:
+        winner_result = "Diana DeGette"
+    elif temp_winner == doane_vote:
+        winner_result = "Raymon Anthony Doane"
+    return winner_result
 
+# save winner
+winner_list = [stockham_vote, degette_vote, doane_vote]
+winner = calc_winner(winner_list)
 
+# create dashes for interesting appearance
+dashes = ""
+for i in range(20):
+    dashes += "-"
 
+# write results to .txt file
+with open(os.path.join("PyPoll", "analysis", "ElectionResults.txt"), "w") as output_text_file:
+    output_text_file.write("Election results")
+    output_text_file.write("\n\n" + dashes)
+    output_text_file.write(f"\n\n Total Votes: {total_votes}")
+    output_text_file.write("\n\n" + dashes)
+    output_text_file.write(f"\n\n Charles Casper Stockham: {str(stockham_percent)}% ({str(stockham_vote)})")
+    output_text_file.write(f"\n\n Diana DeGette: {str(degette_percent)}% ({str(degette_vote)})")
+    output_text_file.write(f"\n\n Raymon Anthony Doane: {str(doane_percent)}% ({str(doane_vote)})")
+    output_text_file.write("\n\n" + dashes)
+    output_text_file.write(f"\n\n Winner: {winner}")
+    output_text_file.write("\n\n" + dashes)
 
-
-
-
-    
-
-
-
+# print results in terminal
+print("\nElection results")
+print("\n" + dashes)
+print(f"\n Total Votes: {total_votes}")
+print("\n" + dashes)
+print(f"\n Charles Casper Stockham: {str(stockham_percent)}% ({str(stockham_vote)})")
+print(f"\n Diana DeGette: {str(degette_percent)}% ({str(degette_vote)})")
+print(f"\n Raymon Anthony Doane: {str(doane_percent)}% ({str(doane_vote)})")
+print("\n" + dashes)
+print(f"\n Winner: {winner}")
+print("\n" + dashes + "\n")
