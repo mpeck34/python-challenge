fb2d6e8ed38b48931438d330289873ccbffcf1ab
mpeck34 <mark.peck34@gmail.com>
2024-03-10 16:52:49 +1100

    nearly done pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -25,15 +25,11 @@ profit_loss_list_header = profit_loss_list.pop(0)
 profit_loss_list_int = [int(x) for x in profit_loss_list]
 
 total_months = len(date_list)
-print(total_months)
 
-print(profit_loss_list)
+total_profit = 0
+for num in profit_loss_list_int:
+    total_profit += int(num)
 
-total_profit = [int(num) for num in profit_loss_list_int]
-#total_profit = 0
-#for num in profit_loss_list_int:
-#    total_profit += int(num)
-print(total_profit)
 
 greatest_profit = 0
 greatest_delta = 0
@@ -54,8 +50,27 @@ for i in range(len(profit_loss_list_int)-1):
             greatest_loss = greatest_loss_delta
             greatest_loss_date = date_list[i+1]
 
+average_change = round((profit_loss_list_int[-1] - profit_loss_list_int[0]) / (len(profit_loss_list_int)-1), 2)
+
+print(total_months)
+print(total_profit)
+print(average_change)
 print(greatest_profit)
 print(greatest_date)
 print(greatest_loss)
 print(greatest_loss_date)
 
+
+output_text_file = open("./PyBank/analysis/FinancialAnalysis.txt","w")
+output_text_file.write("Financial Analysis")
+
+dashes = ""
+for i in range(20):
+    dashes += "-"
+
+output_text_file.write("\n\n" + dashes)
+output_text_file.write(f"\n\n Total Months: {str(total_months)}")
+output_text_file.write(f"\n\n Total: {str(total_profit)}")
+output_text_file.write(f"\n\n Average Change: {str(average_change)}")
+output_text_file.write(f"\n\n Greatest Increase in Profits: {greatest_date} ({greatest_profit})")
+output_text_file.write(f"\n\n Greatest Decrease in Profits: {greatest_loss_date} ({greatest_loss})")
